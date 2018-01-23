4643e5d261e2eb0266962e8311e1794a5fec73d3
Jacob Gonzalez <jgonzalez@atlassian.com>
2018-01-23 11:30:12 +1100

    Added prometheus dep

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
+)
+
+var (
+	counter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "test",
+			Help: "test",
+		},
+	)
 )
 
 func main() {
