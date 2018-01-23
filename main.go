db1dd79084fd0d72b989760e01a9aa77d4be9065
Jacob Gonzalez <jgonzalez@atlassian.com>
2018-01-23 11:57:29 +1100

    Cleaned main

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/prometheus/client_golang/prometheus"
-	log "github.com/sirupsen/logrus"
-	"k8s.io/client-go/kubernetes"
-)
-
-var (
-	counter = prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Name: "test",
-			Help: "test",
-		},
-	)
-)
-
 func main() {
-	sess := session.Must(session.NewSession())
-	log.Printf("%#v", sess.Config.Credentials)
-	log.Printf("%#v", kubernetes.Clientset{})
+
 }
