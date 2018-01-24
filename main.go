1c21febc6486aeaf6e80e50e0db581a8f3002b4c
Jacob Gonzalez <jgonzalez@atlassian.com>
2018-01-24 13:17:59 +1100

    removed test code

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
 	"k8s.io/client-go/kubernetes"
 )
 
 func main() {
-	b := kingpin.Arg("sd", "S").Bool()
 	log.Printf("%#v", kubernetes.Clientset{})
-	log.Println("Hello, world!", b)
+	log.Println("Hello, world!")
 }
