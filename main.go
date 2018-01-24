15081ed792e8e16f3e41c04bd6a989ebd27ca46b
Jacob Gonzalez <jgonzalez@atlassian.com>
2018-01-24 13:15:40 +1100

    Added kingpin dep

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 	"k8s.io/client-go/kubernetes"
 )
 
 func main() {
+	b := kingpin.Arg("sd", "S").Bool()
 	log.Printf("%#v", kubernetes.Clientset{})
-	log.Println("Hello, world!")
+	log.Println("Hello, world!", b)
 }
