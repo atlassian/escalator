d072d650f7789d6cde53fa5515fd61936797c6c5
Jacob Gonzalez <jgonzalez@atlassian.com>
2018-02-16 14:51:14 +1100

    autoscaling dep

--- pkg/cloudprovider/aws/aws.go
+++ pkg/cloudprovider/aws/aws.go
@@ -1,8 +1,17 @@
 package aws
 
-import "github.com/aws/aws-sdk-go/aws/session"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+)
 
 func importStub() {
 	sess := session.Must(session.NewSession())
 	_ = sess
+	svc := autoscaling.New(sess, &aws.Config{
+		Region: aws.String(endpoints.UsEast1RegionID),
+	})
+	_ = svc
 }
