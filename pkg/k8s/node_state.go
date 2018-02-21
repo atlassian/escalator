a96087ffd69d8624476357476370530b0e69142c
Jacob Gonzalez <jgonzalez@atlassian.com>
2018-02-21 16:28:39 +1100

    Added nodeinfo map. Updated deps to use it

--- pkg/k8s/node_state.go
+++ pkg/k8s/node_state.go
@@ -1,8 +1,45 @@
 package k8s
 
-import "k8s.io/api/core/v1"
+import (
+	"k8s.io/api/core/v1"
+	"k8s.io/kubernetes/plugin/pkg/scheduler/schedulercache"
+)
+
+// CreateNodeNameToInfoMap creates a map of schedulercache.NodeInfo which maps node names to nodes and pods to nodes
+// From K8s cluster-autoscaler
+func CreateNodeNameToInfoMap(pods []*v1.Pod, nodes []*v1.Node) map[string]*schedulercache.NodeInfo {
+	nodeNameToNodeInfo := make(map[string]*schedulercache.NodeInfo)
+	for _, pod := range pods {
+		nodeName := pod.Spec.NodeName
+		if _, ok := nodeNameToNodeInfo[nodeName]; !ok {
+			nodeNameToNodeInfo[nodeName] = schedulercache.NewNodeInfo()
+		}
+		nodeNameToNodeInfo[nodeName].AddPod(pod)
+	}
+
+	for _, node := range nodes {
+		if _, ok := nodeNameToNodeInfo[node.Name]; !ok {
+			nodeNameToNodeInfo[node.Name] = schedulercache.NewNodeInfo()
+		}
+		nodeNameToNodeInfo[node.Name].SetNode(node)
+	}
+
+	// Some pods may be out of sync with node lists. Removing incomplete node infos.
+	keysToRemove := make([]string, 0)
+	for key, nodeInfo := range nodeNameToNodeInfo {
+		if nodeInfo.Node() == nil {
+			keysToRemove = append(keysToRemove, key)
+		}
+	}
+	for _, key := range keysToRemove {
+		delete(nodeNameToNodeInfo, key)
+	}
+
+	return nodeNameToNodeInfo
+}
 
 // NodeEmpty returns if the node is empty of pods, except for daemonsets
 func NodeEmpty(node *v1.Node) bool {
+
 	return false
 }
